ef5f775d5d42cc8996989e25d627cea3381e1906
kfirb <kfirb@wix.com>
2016-08-20 15:10:20 +0300

    post support for driver

--- src/test/scala/com/wix/java/one/demo/app/UsersServerDriver.scala
+++ src/test/scala/com/wix/java/one/demo/app/UsersServerDriver.scala
@@ -6,21 +6,36 @@ import io.vertx.core.{Vertx, Handler}
 import scala.concurrent.duration._
 import com.wix.java.one.demo.UsersServerStarter
 
-trait UsersServerDriver {
-  self: VertXClientBase =>
-  def get[T](path: String, assert: HttpClientResponse => T) = {
-    
-    val p = Promise[T]()
-    
-    httpClient.getNow(UsersServerStarter.port, "localhost", path, new Handler[HttpClientResponse] {
-      override def handle(response: HttpClientResponse): Unit = {
-        p success assert(response)
-      }
-    })
+trait TransportBase {
+  
+  def handler[T](p: Promise[T], assert: HttpClientResponse => T) = new Handler[HttpClientResponse] {
+    override def handle(response: HttpClientResponse): Unit = {
+      p success assert(response)
+    }
+  }
 
+  def execute[T](f: Promise[T] => Any) = {
+    val p = Promise[T]()
+    f(p)
     Await.result(p.future, 3.seconds)
   }
 
+}
+
+trait UsersServerDriver extends TransportBase {
+  self: VertXClientBase =>
+  def get[T](path: String, assert: HttpClientResponse => T) = {
+    execute{ p: Promise[T] =>
+      httpClient.getNow(UsersServerStarter.port, "localhost", path, handler(p, assert))
+    }
+  }
+  
+  def post[T](path: String, data: String, assert: HttpClientResponse => T) = {
+    execute { p: Promise[T] =>
+      httpClient.post(UsersServerStarter.port, "localhost", path, handler(p, assert))
+                .end(data)
+    }    
+  }
 }
 
 trait VertXClientBase {
